fc3e0b788ce5f29652a37e3e317011de8fc21e43
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-05-20 10:01:56 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function getage() {
+  let d1 = document.getElementById("date").value;
+  let m1 = document.getElementById("month").value;
+  let y1 = document.getElementById("year").value;
+
+  let date = new Date();
+  let d2 = date.getDate();
+  let m2 = 1 + date.getMonth();
+  let y2 = date.getFullYear();
+  let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (d1 > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+  var d = d2 - d1;
+  var m = m2 - m1;
+  var y = y2 - y1;
+
+  document.getElementById("age").innerHTML =
+    "Your Age is " + y + " Years " + m + " Months " + d + " Days";
+}
